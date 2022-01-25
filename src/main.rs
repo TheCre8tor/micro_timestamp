1283bb91bdd0f7f0df18297ba6b5f9a5e0169358
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-01-25 18:26:45 +0100

    Working on /api endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,40 @@
+use actix_web::web::Json;
+use actix_web::{get, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use std::io;
-use actix_web::{web::{get, Json}, App, HttpRequest, HttpServer, Responder};
-use serde::{Serialize};
 
-
-// Routes -->
-async fn greet(req: HttpRequest) -> impl Responder {
-    println!("Request: {:?}", req.match_info());
-
-    let name = req.match_info().get("uri").unwrap_or("World");
-    format!("Hello {}!", name)
+#[derive(Serialize, Debug)]
+pub struct Timestamp {
+    unix: i64,
+    utc: String,
 }
 
-async fn current_temperature() -> impl Responder {
-    Json( Measurement { temperature: 42.3, likes: true } )
+#[derive(Deserialize)]
+struct DateParam {
+    date: String,
 }
 
-#[derive(Serialize)]
-struct Measurement {
-    temperature: f32,
-    likes: bool
+#[get("/api")]
+async fn default_date() -> impl Responder {
+    let date: DateTime<Utc> = Utc::now();
+    let seconds_to_milli = date.timestamp() * 1000;
+
+    let response = Timestamp {unix: seconds_to_milli, utc: date.to_string()};
+
+    Json(response)
 }
 
-#[actix_web::main]
+#[get("/api/{date}")]
+async fn convert_date(params: web::Path<DateParam>) -> impl Responder {
+    let date: DateTime<Utc> = params.date.parse().unwrap_or(Utc::now());
+    format!("{}", date)
+}
+
+#[tokio::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", get().to(greet))
-            // .route("/{uri}", get().to(greet))
-            .route("/measure", get().to(current_temperature))
-    })
-        .bind(("127.0.0.1", 7070))?
+    HttpServer::new(|| App::new().service(convert_date).service(default_date))
+        .bind("127.0.0.1:7070")?
         .run()
         .await
 }
