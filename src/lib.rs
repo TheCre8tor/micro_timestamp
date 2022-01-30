5fe95291729c67015ef958d2fba54fd68cab1c84
Alexander Nitiola <cre8tor.alexander@gmail.com>
2022-01-30 17:22:29 +0100

    Added application configuration yaml files

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
 pub mod domain;
 pub mod routes;
+pub mod startup;
 pub mod telemetry;
+pub mod configurations;
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,11 @@
-use actix_web::{web, App, HttpServer};
-use micro_timestamp::routes::{convert_date, default_date};
+use micro_timestamp::startup;
 use std::io;
-// use tracing_actix_web::TracingLogger;
+use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
-    HttpServer::new(|| {
-        App::new().service(
-            web::scope("/api")
-                .default_service(web::get().to(default_date))
-                .route("/{date}", web::get().to(convert_date)),
-        )
-    })
-    .bind("127.0.0.1:7070")?
-    .run()
-    .await
+    let address = format!("{}:{}", "127.0.0.1", "7070");
+    let listener = TcpListener::bind(address)?;
+
+    startup::run(listener)?.await
 }
